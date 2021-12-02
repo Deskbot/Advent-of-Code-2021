f5900d4825a4f78aeebfa3de527d47fd863f7c16
Thomas Richards <Deskbot@users.noreply.github.com>
2021-12-02 21:51:49 +0000

    most of day 1 part 2

--- src/main.zig
+++ src/main.zig
@@ -14,6 +14,8 @@ pub fn main() anyerror!void {
     defer file.close();
 
     try part1(&file.reader());
+    try file.seekTo(0);
+    try part2(&file.reader());
 }
 
 fn part1(reader: *File.Reader) !void {
@@ -34,4 +36,57 @@ fn part1(reader: *File.Reader) !void {
     _ = try stdout.print("Part 1 {}\n", .{increases});
 }
 
-fn part2(reader: *File.Reader) !void {}
+fn Window(comptime size: i64) type {
+    return struct {
+        const Self = @This();
+
+        arr: [size]i64,
+        index: usize,
+
+        fn new() Self {
+            return Self{
+                .arr = std.mem.zeroes([size]i64),
+                .index = 0,
+            };
+        }
+
+        fn append(self: *Self, val: i64) void {
+            self.arr[self.index] = val;
+            self.index = (self.index + 1) % size;
+        }
+    };
+}
+
+fn part2(reader: *File.Reader) !void {
+    var window = Window(3).new();
+    var previousWindowSum: ?i64 = null;
+
+    var increases: i64 = 0;
+
+    var buf: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const nextInt = try std.fmt.parseInt(i64, line, 10);
+
+        window.append(nextInt);
+
+        const windowSum = sum(&window.arr);
+
+        if (previousWindowSum != null and windowSum > previousWindowSum.?) {
+            increases += 1;
+        }
+
+        previousWindowSum = windowSum;
+    }
+
+    _ = try stdout.print("Part 2 {}\n", .{increases});
+}
+
+fn sum(arr: []const i64) i64 {
+    var result: i64 = 0;
+
+    for (arr) |elem| {
+        result += elem;
+    }
+
+    return result;
+}
