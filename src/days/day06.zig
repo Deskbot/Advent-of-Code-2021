0ddcc82005657b38b41595d85bce89dfeb8eed22
Thomas Richards <Deskbot@users.noreply.github.com>
2021-12-11 19:43:10 +0000

    day 6 part 1 inefficient maybe solution

---
+++ src/days/day06.zig
@@ -0,0 +1,107 @@
+const std = @import("std");
+
+const debug = std.debug;
+const fmt = std.fmt;
+const fs = std.fs;
+const os = std.os;
+const io = std.io;
+const mem = std.mem;
+
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+const File = std.fs.File;
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
+
+const filter = @import("../utils/filter.zig").filter;
+const arr = @import("../utils/arr.zig");
+
+const stdout = std.io.getStdOut().writer();
+
+pub fn day06() !void {
+    var gpa = GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const myIput = [_]i64{ 1, 2, 5, 1, 1, 4, 1, 5, 5, 5, 3, 4, 1, 2, 2, 5, 3, 5, 1, 3, 4, 1, 5, 2, 5, 1, 4, 1, 2, 2, 1, 5, 1, 1, 1, 2, 4, 3, 4, 2, 2, 4, 5, 4, 1, 2, 3, 5, 3, 4, 1, 1, 2, 2, 1, 3, 3, 2, 3, 2, 1, 2, 2, 3, 1, 1, 2, 5, 1, 2, 1, 1, 3, 1, 1, 5, 5, 4, 1, 1, 5, 1, 4, 3, 5, 1, 3, 3, 1, 1, 5, 2, 1, 2, 4, 4, 5, 5, 4, 4, 5, 4, 3, 5, 5, 1, 3, 5, 2, 4, 1, 1, 2, 2, 2, 4, 1, 2, 1, 5, 1, 3, 1, 1, 1, 2, 1, 2, 2, 1, 3, 3, 5, 3, 4, 2, 1, 5, 2, 1, 4, 1, 1, 5, 1, 1, 5, 4, 4, 1, 4, 2, 3, 5, 2, 5, 5, 2, 2, 4, 4, 1, 1, 1, 4, 4, 1, 3, 5, 4, 2, 5, 5, 4, 4, 2, 2, 3, 2, 1, 3, 4, 1, 5, 1, 4, 5, 2, 4, 5, 1, 3, 4, 1, 4, 3, 3, 1, 1, 3, 2, 1, 5, 5, 3, 1, 1, 2, 4, 5, 3, 1, 1, 1, 2, 5, 2, 4, 5, 1, 3, 2, 4, 5, 5, 1, 2, 3, 4, 4, 1, 4, 1, 1, 3, 3, 5, 1, 2, 5, 1, 2, 5, 4, 1, 1, 3, 2, 1, 1, 1, 3, 5, 1, 3, 2, 4, 3, 5, 4, 1, 1, 5, 3, 4, 2, 3, 1, 1, 4, 2, 1, 2, 2, 1, 1, 4, 3, 1, 1, 3, 5, 2, 1, 3, 2, 1, 1, 1, 2, 1, 1, 5, 1, 1, 2, 5, 1, 1, 4 };
+
+    _ = try stdout.write("Day 06\n");
+
+    _ = try stdout.print("Part 1 {}\n", .{try part1(&myIput, &gpa.allocator)});
+    // _ = try stdout.print("Part 2 {}\n", .{try part2(input, &gpa.allocator)});
+}
+
+fn part1(input: []const i64, allocator: *Allocator) !i64 {
+    var fish = try ArrayList(LanternFish).initCapacity(allocator, input.len);
+    defer fish.deinit();
+
+    for (input) |num| {
+        try fish.append(LanternFish.new(num));
+    }
+
+    var school = School.new(fish, allocator);
+    defer school.deinit();
+
+    var day: i64 = 0;
+
+    while (day <= 80) {
+        day += 1; // start at day 1
+
+        try school.passOneDay();
+    }
+
+    return @intCast(i64, school.size());
+}
+
+const LanternFish = struct {
+    timer: i64,
+
+    pub fn new(intialTimer: i64) @This() {
+        return @This(){
+            .timer = intialTimer,
+        };
+    }
+
+    // returns whether the fish gave birth
+    pub fn passOneDay(lanternFish: *LanternFish) ?LanternFish {
+        // increment the timer
+        if (lanternFish.timer == 0) {
+            lanternFish.timer = 6;
+        } else {
+            lanternFish.timer += 1;
+        }
+
+        return @This().new(8);
+    }
+};
+
+const School = struct {
+    allocator: *Allocator,
+    fish: ArrayList(LanternFish),
+
+    pub fn new(fish: ArrayList(LanternFish), allocator: *Allocator) @This() {
+        return @This(){
+            .allocator = allocator,
+            .fish = fish,
+        };
+    }
+
+    pub fn deinit(school: *@This()) void {
+        school.fish.deinit();
+    }
+
+    pub fn passOneDay(school: *@This()) !void {
+        var newFishArr = ArrayList(LanternFish).init(school.allocator);
+        defer newFishArr.deinit();
+
+        for (school.fish.items) |*fish| {
+            if (fish.passOneDay()) |newFish| {
+                try newFishArr.append(newFish);
+            }
+        }
+
+        try school.fish.appendSlice(newFishArr.items);
+    }
+
+    pub fn size(school: @This()) usize {
+        return school.fish.items.len;
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const day02 = @import("./days/day02.zig").day02;
 const day03 = @import("./days/day03.zig").day03;
 const day04 = @import("./days/day04.zig").day04;
 const day05 = @import("./days/day05.zig").day05;
+const day06 = @import("./days/day06.zig").day06;
 
 pub fn main() anyerror!void {
     try day01();
@@ -10,4 +11,5 @@ pub fn main() anyerror!void {
     try day03();
     try day04();
     try day05();
+    try day06();
 }
